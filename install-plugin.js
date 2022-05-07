8493b6ebbe551aa944e6ded28e576596b1a5503a
josePereiro <jose.pereiro.personal@gmail.com>
2022-05-07 15:29:32 -0400

    Trigger signal working

---
+++ install-plugin.js
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require("path")
+const CProc = require('child_process');
+
+const target_vault = process.argv[2]
+if (!target_vault) { throw {msg: "target vault path not defined"}; }
+if (!fs.existsSync(target_vault)) { throw {msg: "target vault path not found"}; }
+const src_plugin_dir = process.cwd()
+const src_plugin_name = path.basename(src_plugin_dir)
+
+// ------------------------------------------------------------------------
+// Utils
+function log_newsection(msg){
+    console.log();
+    console.log("------------------------------------------------------------------------");
+    console.log(msg);    
+}
+
+// ------------------------------------------------------------------------
+log_newsection("Params");
+console.log("src_plugin_dir: ", src_plugin_dir)
+console.log("src_plugin_name: ", src_plugin_name)
+console.log("target_vault: ", target_vault)
+
+const obsidian_dotfolder = path.join(target_vault, ".obsidian")
+if (!fs.existsSync(obsidian_dotfolder))  { throw {msg: ".obsidian folder not found"}; }
+
+const obsidian_plugin_folder = path.join(obsidian_dotfolder, "plugins")
+const obsidian_target_plugin_folder = path.join(obsidian_plugin_folder, src_plugin_name)
+if (!fs.existsSync(obsidian_target_plugin_folder)) {
+    fs.mkdirSync(obsidian_target_plugin_folder, { recursive: true })
+}
+console.log("obsidian_target_plugin_folder: ", obsidian_target_plugin_folder)
+
+// ------------------------------------------------------------------------
+function run_compile(cmd) {
+    log_newsection("compiling ts");
+    CProc.execSync(cmd, function callback(error, stdout, stderr) {
+        if (error) throw error;
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+
+// ------------------------------------------------------------------------
+function copy_file() {
+
+    log_newsection("copying files");
+    const tocopy = ["dist/main.js", "dist/styles.css", "manifest.json"]
+    tocopy.forEach(function (file, idx) {
+            var src = path.join(src_plugin_dir, file)
+        var dest = path.join(obsidian_target_plugin_folder, path.basename(file))
+        fs.copyFile(src, dest, (err) => {
+            if (err) throw err;
+            console.log(file, ' -> ', dest);
+        });
+    });
+}
+
+// ------------------------------------------------------------------------
+function updating_community_plugins_json() {
+    log_newsection("updating community-plugins.json");
+    
+    const obsidian_community_plugins_json = path.join(obsidian_dotfolder, "community-plugins.json")
+    
+    //read
+    let filestr = fs.readFileSync(obsidian_community_plugins_json);
+    let plugins = JSON.parse(filestr);   
+    plugins.push(src_plugin_name)
+    
+    plugins = [...new Set(plugins)]
+    
+    //write
+    filestr = JSON.stringify(plugins);
+    fs.writeFileSync(obsidian_community_plugins_json, filestr);
+    console.log(filestr);
+}
+
+// ============================================================================
+run_compile('npm run dev production')
+copy_file()
+updating_community_plugins_json()
---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import { FileSystemAdapter, Notice, Plugin } from 'obsidian';
+import { existsSync, writeFileSync, mkdirSync } from 'fs';
+import { dirname } from 'path';
+
+// Add a simple command 'Signal backend' which update a file in the plugin folder 
+// that can be use as trigger for different backends
+
+export default class ObA extends Plugin {
+
+	async onload() {
+
+		// This adds a simple command that can be triggered anywhere
+		this.addCommand({
+			id: 'oba-plugin',
+			name: 'Signal backend',
+			callback: () => {
+				setTimeout(() => { 
+					const signal_id = randstring(SIGNAL_LENGTH)
+					const signal_file = this.getSignalPath();
+					const plugin_dir = dirname(signal_file);
+					new Notice("Oba: backend signaled");
+					if (!existsSync(plugin_dir)) {
+						mkdirSync(plugin_dir, { recursive: true })
+					}
+					console.log("backend signaled. file: ", signal_file)
+					console.log("backend signaled. id: ", signal_id)
+					console.log("backend signaled. dirname: ", plugin_dir)
+					writeFileSync(signal_file, signal_id)
+				}, SIGNAL_DELAY);
+			}
+		});
+	}
+
+	onunload() {
+
+	}
+
+	// https://github.com/tillahoffmann/obsidian-jupyter/blob/e1e28db25fd74cd16844b37d0fe2eda9c3f2b1ee/main.ts#L175
+	getSignalPath(): string {
+		const fileName = "trigger-signal.json"
+		let basePath;
+		// base path
+		if (this.app.vault.adapter instanceof FileSystemAdapter) {
+			basePath = this.app.vault.adapter.getBasePath();
+		} else {
+			throw new Error('Cannot determine base path.');
+		}
+		// relative path
+		const relativePath = `${this.app.vault.configDir}/plugins/oba-plugin/${fileName}`;
+		// absolute path
+		return `${basePath}/${relativePath}`;
+	}
+}
+
+// ------------------------------------------------------------------
+// Utils
+const SIGNAL_LENGTH = 8;
+const SIGNAL_DELAY = 300; // let obsidian to save
+const CHARACTERS  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function randstring(length: number): string {
+	if (length < 1) { length = 1; }
+    let result = '';
+    const charactersLength = CHARACTERS.length;
+    for ( let i = 0; i < length; i++ ) {
+        const rinx = Math.floor(Math.random() * charactersLength)
+        result += CHARACTERS.charAt(rinx);
+    }
+   return result;
+}
